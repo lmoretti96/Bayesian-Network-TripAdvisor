8c1912661d17c81968bdbfb08cbfcac05557fb86
Riccardo Nigrelli <20014421@studenti.uniupo.it>
2019-07-06 11:45:34 +0200

    actual state: our model in baesyan, remove some keyword and metadata

--- bayesian.py
+++ bayesian.py
@@ -12,23 +12,28 @@ def format_data(df):
     result = []
     for row in df.itertuples():
         #print(row.Pclass)
-        result.append(dict(great = row.great, good = row.good, clean = row.clean,  comfortable = row.comfortable,
-        beautiful = row.beautiful, wonderful = row.wonderful,  fantastic = row.fantastic, bad = row.bad, 
-         Cleanliness= row.Cleanliness, Location=row.Location ,Businessservice=row.Businessservice,
-           Checkin=row.Checkin, Service=row.Service, Rooms=row.Rooms, Value=row.Value, Overall=row.Overall ))
+        result.append(dict(
+          great = row.great, good = row.good, clean = row.clean,  comfortable = row.comfortable,
+          bad = row.bad, old = row.old,
+          Cleanliness= row.Cleanliness, Location=row.Location, Service=row.Service, Rooms=row.Rooms, 
+          Value=row.Value, Overall=row.Overall 
+        ))
+
+            # beautiful = row.beautiful, wonderful = row.wonderful,     
+            # Checkin=row.Checkin, Businessservice=row.Businessservice,
              #      result.append(dict(great = row.great, good = row.good, nice = row.nice, clean = row.clean, helpful = row.helpful, comfortable = row.comfortable,
        # beautiful = row.beautiful, wonderful = row.wonderful, friendly = row.friendly, fantastic = row.fantastic, bad = row.bad, 
       #   Cleanliness= row.Cleanliness, Location=row.Location ,Businessservice=row.Businessservice,
        #    Checkin=row.Checkin, Service=row.Service, Rooms=row.Rooms, Value=row.Value, Overall=row.Overall ))
     return result
 #load all preprocessed training data
-df = pd.read_csv('./features_filtrato.csv', sep=',')
+df = pd.read_csv('./features.csv', sep=',')
 #format data to let them correctly processed by libpgm functions
 node_data = format_data(df)
 
 skel = GraphSkeleton()
 #load structure of our net
-skel.load("./skel-learned.txt")
+skel.load("./our-skel.txt")
 #setting the topologic order
 skel.toporder()
 #learner which will estimate parameters e if needed net structure
@@ -36,7 +41,8 @@ learner = PGMLearner()
 
 #estismting parameters for our own model
 res = learner.discrete_mle_estimateparams(skel, node_data)
-print(str(res))
+print(json.dumps(res.E, indent=2))
+print(json.dumps(res.Vdata, indent=2))
 
 
 #estimating net structure given training data and paramenters this is an alternative to create a new model on our data
@@ -46,15 +52,12 @@ print(str(res))
 #res = learner.discrete_mle_estimateparams(net, node_data)
 #print(str(res))
 
-
-
-
 #compute performances for each oveall score
 for score in range(1,6):
     target = []
     pred = []
     #load testing dataset into a dataframe
-    testdf = pd.read_csv("test-filtrato.csv",  sep = ",")
+    testdf = pd.read_csv("test_filtrato.csv",  sep = ",")
     #selecting row with a certain overall value
     testdf = testdf[testdf["Overall"] == score]
     print(len(testdf))
@@ -65,15 +68,16 @@ for score in range(1,6):
         great = int(testdf.iloc[i]["great"])
         good = int(testdf.iloc[i]["good"])
         comfortable = int(testdf.iloc[i]["comfortable"])
-        small = int(testdf.iloc[i]["small"])
+        clean = int(testdf.iloc[i]["clean"])
+        # small = int(testdf.iloc[i]["small"])
         bad = int(testdf.iloc[i]["bad"])
         old = int(testdf.iloc[i]["old"])
         Rooms = int(testdf.iloc[i]["Rooms"])
         Location = int(testdf.iloc[i]["Location"])
         Service = int(testdf.iloc[i]["Service"])
         Cleanliness = int(testdf.iloc[i]["Cleanliness"])
-        CheckIn = int(testdf.iloc[i]["CheckIn"])
-        Businessservice = int(testdf.iloc[i]["Businessservice"])
+        #CheckIn = int(testdf.iloc[i]["CheckIn"])
+        #Businessservice = int(testdf.iloc[i]["Businessservice"])
         Value = int(testdf.iloc[i]["Value"])
         Overall = int(testdf.iloc[i]["Overall"])
         #append the overall score to the target list
@@ -82,7 +86,7 @@ for score in range(1,6):
         a = TableCPDFactorization(res)
         #compute the query and evidences as dicts
         query = dict(Overall=Overall)
-        evidence = dict(CheckIn=CheckIn,Businessservice=Businessservice,Service = Service, Location = Location, Cleanliness = Cleanliness, Value = Value, bad = bad, good = good, Rooms = Rooms, great =great, comfortable = comfortable, small = small, old = old)
+        evidence = dict(Service = Service, Location = Location, Cleanliness = Cleanliness, Value = Value, bad = bad, good = good, Rooms = Rooms, great =great, comfortable = comfortable, old = old)
         #run the query given evidence
         result = a.condprobve(query, evidence)
         #choose the max probability ditribution as model prediction
--- filter_csv.py
+++ filter_csv.py
@@ -3,5 +3,5 @@
 import csv
 
 reader = csv.reader(open(r"test.csv"),delimiter=',')
-filtered = filter(lambda p: '-1' != p[6] and  '-1' != p[7] and  '-1' != p[8] and '-1' != p[9] and  '-1' != p[10] and  '-1' != p[11] and  '-1' != p[12] and  '-1' != p[13]   , reader)
+filtered = filter(lambda p: '-1' != p[7] and  '-1' != p[8] and  '-1' != p[9] and '-1' != p[10] and  '-1' != p[11] , reader)
 csv.writer(open(r"test_filtrato.csv",'w'),delimiter=',').writerows(filtered)
